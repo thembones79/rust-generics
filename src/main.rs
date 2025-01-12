94307dc7d6345ff048ff30a93ea69dd9aae4f70f
thembones79 <michal.szumnarski@gmail.com>
2025-01-12 20:29:22 +0100

    rust generics

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use num_traits::ToPrimitive;
+use num_traits::{ ToPrimitive ,Float};
 
-fn solve(a: f64, b: f64) -> f64 {
+fn solve<T: Float>(a: T, b: T) -> f64 {
     let a_f64 = a.to_f64().unwrap();
     let b_f64 = b.to_f64().unwrap();
     (a_f64.powi(2) + b_f64.powi(2)).sqrt()
@@ -9,6 +9,5 @@ fn main() {
     let a: f32 = 3.0;
     let b: f32 = 4.0;
 
-
-    println!("{}", solve(a, b));
+    println!("{}", solve::<f32>(a, b));
 }
